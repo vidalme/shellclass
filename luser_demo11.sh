6a81a07517ec756bf29085f2685d153a6fd8a2f6
andre <andrinho@gmail.com>
2023-12-01 15:02:40 +0000

    update course

--- luser_demo11.sh
+++ luser_demo11.sh
@@ -5,10 +5,69 @@
 #Verbose mode can be enabled with -v.
 #
 #Set a default password length
-LENGHT=48
+LENGTH=48
+
+usage() {
+	echo 'Usage: ${0} [-vs] [-l LENGTH]' >&2
+	echo 'Generate a random password.'
+	echo '  -l LENGTH Specify the password length.'
+	echo '  -s        Append a special character to the password.'
+	echo '  -v        Increase verbosity.'
+	exit 1
+}
+
+log(){
+	local MESSAGE="${@}"
+	if [[ "${VERBOSE}" = 'true' ]]
+	then
+		echo '${MESSAGE}'
+	fi
+}
 
 while getopts vl:s OPTION
 do
 	case ${OPTION} in
 		v)
-			VERBOSESE
+			VERBOSE='true'
+			log 'Verbose mode on.'
+			;;
+		l)
+			LENGTH="${OPTARG}"
+			;;
+		s)
+			USE_SPECIAL_CHARACTER='true'
+			;;
+		?)
+			usage
+			;;
+	esac
+done
+
+# Remove the options while leaving the remaining arguments.
+# shift "$(( OPTIND - 1 ))"
+#
+#
+# if [[ "${#}" -gt 0 ]]
+# then
+#	 usage
+# fi
+
+log 'Generating a password.'
+
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c"${LENGTH}" )
+
+# Append a special character if requested to do so.
+if [[ "${USE_SPECIAL_CHARACTER}" = 'true' ]]
+then
+	log 'Selecting a random special character.'
+	SPECIAL_CHARACTER=$(echo '!@#$%¨&*()_+-=' | fold -w1 | shuf | head -c1)
+	PASSWORD="${PASSWORD}${SPECIAL_CHARACTER}"
+fi
+
+log 'Done.'
+log 'Here is the password:'
+
+#Display the password
+echo "${PASSWORD}"
+
+exit 0
